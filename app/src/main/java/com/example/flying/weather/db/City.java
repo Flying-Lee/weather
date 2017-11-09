a729ca1fec74c8cecfabeb45f1300c82fa7f1960
李鹏飞 <iamflying@vip.qq.com>
2017-11-09 10:08:15 +0800

    创建数据库和表

---
+++ app/src/main/java/com/example/flying/weather/db/City.java
@@ -0,0 +1,60 @@
+package com.example.flying.weather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by flying on 2017/11/9.
+ */
+
+public class City extends DataSupport {
+
+    private int id;
+
+    private String cityName;
+
+    private int cityCode;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", cityName='" + cityName + '\'' +
+                ", cityCode=" + cityCode +
+                ", provinceCode=" + provinceCode +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/example/flying/weather/db/County.java
@@ -0,0 +1,71 @@
+package com.example.flying.weather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by flying on 2017/11/9.
+ */
+
+public class County extends DataSupport {
+
+    private int id;
+
+    private String countyName;
+
+    private String weatherId;
+
+    private int cityId;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    @Override
+    public String toString() {
+        return "County{" +
+                "id=" + id +
+                ", countyName='" + countyName + '\'' +
+                ", weatherId='" + weatherId + '\'' +
+                ", cityId=" + cityId +
+                ", provinceCode=" + provinceCode +
+                '}';
+    }
+}
